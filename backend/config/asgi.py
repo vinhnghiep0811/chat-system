a0825eab6b6b554dac118887f79c0ca165d417f2
lvn <vinhnghieple0811@gmail.com>
2026-03-02 22:03:53 +0700

    fix for websocket deploy

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,34 +1,23 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import OriginValidator
 
+from chat.ws_auth import JwtAuthMiddlewareStack
+from chat.routing import websocket_urlpatterns
 
 django_asgi_app = get_asgi_application()
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-from chat.ws_auth import JwtAuthMiddlewareStack
-
-from chat.routing import websocket_urlpatterns  # đổi "chat" nếu app tên khác
-
-
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(
+    "websocket": OriginValidator(
         JwtAuthMiddlewareStack(
             URLRouter(websocket_urlpatterns)
-        )
+        ),
+        [
+            "https://chat-system-bay.vercel.app",
+        ],
     ),
 })
