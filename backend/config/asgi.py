fbc42e59abde8358b94444df1f67f86991f33e7c
lvn <vinhnghieple0811@gmail.com>
2026-03-02 22:16:17 +0700

    fix

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -2,13 +2,15 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()  # MUST be before importing consumers/routing
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import OriginValidator
 
 from chat.ws_auth import JwtAuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
 
-django_asgi_app = get_asgi_application()
+# Import routing AFTER Django setup is ready
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
@@ -18,6 +20,6 @@ application = ProtocolTypeRouter({
         ),
         [
             "https://chat-system-bay.vercel.app",
-        ],
+        ]
     ),
 })
